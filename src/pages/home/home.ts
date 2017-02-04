804340b5bce6f06097192f39166e194f79aa8ad5
Pornmongkon Pongsai <pornmongkonp@riverpark.co.th>
2017-02-04 23:12:24 +0700

    Test add plugin cordova-plugin-android-native-pdfviewer

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+declare var AndroidNativePdfViewer: any
 
 @Component({
   selector: 'page-home',
@@ -11,5 +12,20 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  openPdfFromUrl() {
+        var options = {
+          headerColor: "#2196F3",
+          showScroll: false,
+          swipeHorizontal: false
+        };
+        var url = "https://education.github.com/git-cheat-sheet-education.pdf"; // <==== url pdf
+        AndroidNativePdfViewer.openPdfUrl(url,
+          "Test",
+          options,
+          function (success) {
+            console.log("AndroidNativePdfViewer success");
+          }, function (error) {
+            console.error("AndroidNativePdfViewer error : " + error);
+          });
+  }
 }
